5f41fa5524b83d71bf46e6af35a91717c2c277d3
Jacob <jacob@Jacobs-MacBook-Pro-4.local>
2018-10-21 18:40:57 -0400

    more bug fix

--- demo_flask.py
+++ demo_flask.py
@@ -84,7 +84,7 @@ def get_gcal_service():
 @app.route('/authorize')
 def authorize():
   # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
-  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_config(
+  flow = google_auth_oauthlib.flow.Flow.from_client_config(
       client_secrets_config(), scopes=SCOPES)
 
   flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
@@ -108,7 +108,7 @@ def oauth2callback():
   # verified in the authorization server response.
   state = flask.session['state']
 
-  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_config(
+  flow = google_auth_oauthlib.flow.Flow.from_client_config(
       client_secrets_config(), scopes=SCOPES, state=state)
   flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
 
